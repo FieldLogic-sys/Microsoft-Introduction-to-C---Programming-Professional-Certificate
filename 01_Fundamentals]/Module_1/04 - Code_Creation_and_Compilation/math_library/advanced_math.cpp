634259ce19b3b4c7c0521a0c8742c44e3b44176c
Tony Aldea <tonythecoder@outlook.com>
2026-01-12 20:44:45 -0500

    Implement math library functions: Add basic, advanced, and geometry math operations with corresponding tests in main.cpp

---
+++ 01_Fundamentals]/Module_1/04 - Code_Creation_and_Compilation/math_library/advanced_math.cpp
@@ -0,0 +1,14 @@
+#include "advanced_math.h"
+#include <cmath> // For sqrt function
+// TODO: Implement the power function
+double power(double base, int exponent) {
+    // Your implementation here    
+}
+// TODO: Implement the squareRoot function
+double squareRoot(double number) {
+    // Your implementation here    
+}
+// TODO: Implement the absolute function
+double absolute(double number) {
+    // Your implementation here    
+}
