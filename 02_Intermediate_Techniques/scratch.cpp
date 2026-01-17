46d35707777f3ed18c040da80954f4a81e819dc9
Tony Aldea <tonythecoder@outlook.com>
2026-01-16 21:14:01 -0500

    Enhance matrix manipulation: Add multiplication, transpose, and determinant functions; improve error handling in main

--- 02_Intermediate_Techniques/scratch.cpp
+++ 02_Intermediate_Techniques/scratch.cpp
@@ -2,13 +2,32 @@
 #include <iostream>
 
 int main() {
-    std::array <int, 5>  numbers = {1, 2, 3, 4, 5}; 
-    int element = numbers.at(3);
-    std::cout << "Element at index 3: " << element << std::endl; 
+    // std::array <int, 5>  numbers = {1, 2, 3, 4, 5}; 
+    // int element = numbers.at(3);
+    // std::cout << "Element at index 3: " << element << std::endl; 
     
-    for (int arr = 0; arr < numbers.size(); ++arr) {
-        std::cout << "Element at index " << arr << ": " << numbers.at(arr) << std::endl;
-    }
+    // for (int arr = 0; arr < numbers.size(); ++arr) {
+    //     std::cout << "Element at index " << arr << ": " << numbers.at(arr) << std::endl;
+    // }
 
-    return 0;
+    // return 0;
+
+
+    std::string statement = "The quick brown fox";
+    std::cout << statement << std::endl;
+size_t position = statement.find("brown"); // Locates substring
+statement.replace(position, 5, "white"); // Modifies substring
+
+std::cout << statement << std::endl; // Outputs: The quick white fox
+
+
+
+std::string unicodeText = u8"你好，世界"; // "Hello, World" in Chinese
+std::cout << unicodeText << std::endl;
+
+
+
+
+
+return 0;
 }
