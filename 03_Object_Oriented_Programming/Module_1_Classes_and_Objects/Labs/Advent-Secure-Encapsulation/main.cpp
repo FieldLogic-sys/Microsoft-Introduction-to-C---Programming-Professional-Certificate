7fad31a1517cfa7f8dac37131c1bbdb54187ab4d
Tony Aldea <tony@tonythecoder.com>
2026-01-31 12:40:38 -0500

    fix(Character): Correct function declaration and formatting for berserkerAttack and berserkerMode

--- 03_Object_Oriented_Programming/Module_1_Classes_and_Objects/Labs/Advent-Secure-Encapsulation/main.cpp
+++ 03_Object_Oriented_Programming/Module_1_Classes_and_Objects/Labs/Advent-Secure-Encapsulation/main.cpp
@@ -26,9 +26,7 @@ public:
         else
             hitPoints = hp;
     }
-    friend void berserkerAttack(Character& c);
-
-
+    friend void berserkerAttack(Character &c);
 
     void setAttackStrength(int attack)
     {
@@ -47,27 +45,42 @@ public:
                   << ", Attack: " << attackStrength << std::endl;
     }
 
-
     // The Friend Fuction needs to be outside of the class which is why I had an error.
-    void berskerAttack(Character& c){
+    void berskerAttack(Character &c)
+    {
         c.hitPoints = 150;
         c.attackStrength += 100;
-        std::cout << "You have entered bersker mode. Get ready. Your attack has been increased to: " << attackStrength <<"." << std::endl;
+        std::cout << "You have entered bersker mode. Get ready. Your attack has been increased to: " << attackStrength << "." << std::endl;
     }
     // Missing this is what caused my error
-    friend void berserkerMode(Character& c);
+    friend void berserkerMode(Character &c);
 };
 
-void berserkerMode(Character& c){
-        c.hitPoints = 150;
-        c.attackStrength += 100;
-        std::cout << "\n[!] "<< c.name << "You have entered bersker mode. Get ready. Your attack has been increased to: " << c.attackStrength <<"." << std::endl;
-    }
+void berserkerMode(Character &c)
+{
+    c.hitPoints = 150;
+    c.attackStrength += 100;
+    std::cout << "\n[!] " << c.name << "You have entered bersker mode. Get ready. Your attack has been increased to: " << c.attackStrength << "." << std::endl;
+}
 int main()
 {
     Character programmer("FieldLogic-dev", 10, 10);
     programmer.displayInfo();
     berserkerMode(programmer);
     programmer.displayInfo();
+
+    // TEST A: Trying to bypass the Setter limit
+    std::cout << "\nAttempting to set HP to 999 via Setter..." << std::endl;
+    programmer.setHitPoints(999);
+    programmer.displayInfo(); // Should be capped at 100
+// TEST A: Trying to bypass the Setter limit
+std::cout << "\nAttempting to set HP to 999 via Setter..." << std::endl;
+programmer.setHitPoints(999); 
+programmer.displayInfo(); // Should be capped at 100
+
+
+// TEST B: Trying to touch private data directly (The "Illegal" Move)
+// programmer.hitPoints = 500; 
+    // IDE Warning `member "Character::hitPoints" (declared at line 8) is inaccessible`
     return 0;
 }
