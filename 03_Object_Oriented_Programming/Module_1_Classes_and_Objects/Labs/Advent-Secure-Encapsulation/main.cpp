19af38d0ba18a608779fa28ddb348fa4337b8874
Tony Aldea <tony@tonythecoder.com>
2026-01-31 12:21:21 -0500

    refactor(Character): Improve formatting and consistency in member functions

--- 03_Object_Oriented_Programming/Module_1_Classes_and_Objects/Labs/Advent-Secure-Encapsulation/main.cpp
+++ 03_Object_Oriented_Programming/Module_1_Classes_and_Objects/Labs/Advent-Secure-Encapsulation/main.cpp
@@ -7,30 +7,37 @@ private:
     std::string name;
     int hitPoints;
     int attackStrength;
+
 public:
-
-    Character(std::string name, int hp, int attack) :
-        name(name), hitPoints(hp), attackStrength(attack) {}
-
+    Character(std::string name, int hp, int attack) : name(name), hitPoints(hp), attackStrength(attack) {}
+    int getHitPoints() const { return hitPoints; }
+    int getAttackStrength() const { return attackStrength; }
 
     void setHitPoints(int hp)
     {
-        if (hp < 0) {
+        if (hp < 0)
+        {
             hp = 0;
         }
-        else if (hp > 100) {
+        else if (hp > 100)
+        {
             hp = 100;
         }
-        else hitPoints = hp;
-        
+        else
+            hitPoints = hp;
     }
-   
+    friend void berserkerAttack(Character& c);
 
-    void setAttackStrength(int attack) {
-        if (attack < 0) {
+
+
+    void setAttackStrength(int attack)
+    {
+        if (attack < 0)
+        {
             attack = 0;
         }
-        else {
+        else
+        {
             attackStrength = attack;
         }
     }
@@ -41,13 +48,19 @@ public:
     }
 
 
-    int getHitPoints() const {return hitPoints;}
-    int getAttackStrength() const { return attackStrength;}
+    // The Friend Fuction needs to be outside of the class which is why I had an error.
+    void berskerAttack(Character& c){
+        c.hitPoints = 150;
+        c.attackStrength += 100;
+        std::cout << "You have entered bersker mode. Get ready. Your attack has been increased to: " << attackStrength <<"." << std::endl;
+    }
 };
 
-int main() {
-Character programmer ("FieldLogic-dev", 10, 10);
+int main()
+{
+    Character programmer("FieldLogic-dev", 10, 10);
     programmer.displayInfo();
-
-return 0;
+    programmer.berskerAttack();
+    programmer.displayInfo();
+    return 0;
 }
